c92dc64f80c442be4636a02f659312f5d0ba5e41
grantholly <gdholly@gmail.com>
2018-05-11 15:55:26 -0700

    setting up first views and serializers

---
+++ api/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from api.models import Transaction, TransactionDetails, StatementOfOrg,
+                       Payee, ElectionActivity, Donor, CommitteesList,
+                       CommitteeHistory, Ballots
+
+class TransactionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Transactions
+        fields = '__all__'
+
+class TransactionDetailSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = TransactionDetails
+        fields = '__all__'
+
+class StatementOfOrgSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = StatementOfOrg
+        fields = '__all__'
+
+class PayeeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Payee
+        fields = '__all__'
+
+class ElectionActivitySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ElectionActivity
+        fields = '__all__'
+
+class DonorSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Donor
+        fields = '__all__'
+
+class CommitteesListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CommitteesList
+        fields = '__all__'
+
+class CommitteeHistorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CommitteeHistory
+        fields = '__all__'
+
+class BallotSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Ballots
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from api.models import (Transactions,
+                        TransactionDetails,
+                        StatementOfOrg,
+                        Payee,
+                        ElectionActivity,
+                        Donor,
+                        CommitteesList,
+                        CommitteeHistory,
+                        Ballots,)
+from api.serializers import (TransactionSerializer, 
+                            TransactionDetailSerializer,
+                            StatementOfOrgSerializer, 
+                            PayeeSerializer,
+                            ElectionActivitySerializer, 
+                            DonorSerializer,
+                            CommitteesListSerializer, 
+                            CommitteeHistorySerializer,
+                            BallotSerializer,)
 
-# Create your views here.
+from rest_framework.decorators import api_view, detail_route
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
